d22da2146eef481ec78548791399ec5ce18bfdb7
Wagner dos Santos Lima <wslmacieira@PC.localdomain>
2020-03-01 14:21:36 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocketseat' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Wagner dos Santos',
+    email: 'wslmacieira@outlook.com',
+    password_hash: '123456',
+  });
+  return res.json(user);
+});
 
 export default routes;
