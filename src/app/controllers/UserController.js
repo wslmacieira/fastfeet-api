e6b1a204905005b84fa60b8bb4c8a88f0d2e09d8
Wagner dos Santos Lima <wslmacieira@PC.localdomain>
2020-03-01 16:36:55 -0300

    Criando UserController metodo store

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,16 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(401).json({ error: 'User already exists' });
+    }
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({ id, name, email });
+  }
+}
+
+export default new UserController();
