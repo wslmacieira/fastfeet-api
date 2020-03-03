fe93d4b506dbad965a75299088007e96179ac235
Wagner dos Santos Lima <wslmacieira@PC.localdomain>
2020-03-03 01:28:47 -0300

    Criando Recipient model; RecipientController

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,86 @@
+import * as Yup from 'yup';
+
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json(recipient);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip_code: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findOne({
+      where: { id },
+    });
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exist' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    });
+  }
+}
+
+export default new RecipientController();
