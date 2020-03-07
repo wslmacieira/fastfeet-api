70442139df4c26f50fd364814d26d018316f971c
Wagner dos Santos Lima <wslmacieira@PC.localdomain>
2020-03-07 12:49:30 -0300

    Configurando Mongodb connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
@@ -13,6 +14,7 @@ const models = [User, Recipient, Deliveryman, File, Delivery];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -20,6 +22,17 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/fastfeet',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
